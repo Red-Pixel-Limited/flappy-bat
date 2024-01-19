dabe67070d6ef9aa611cf0d0eb92d4608f4c21a7
Raune <128294978+anana243@users.noreply.github.com>
2024-01-19 23:22:24 +0000

    login window so far

--- screens/login.py
+++ screens/login.py
@@ -9,14 +9,37 @@
 
 # 2. 
 
-import pygame
-import pygame_gui
+from tkinter import *
 
-pygame.init()
+def register():
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+    
+    username = StringVar()
+    password = StringVar()
+    
+    Label(screen1, text = "Please enter details").pack()
+    Label(screen1, text = "").pack()
+    Label(screen1, text = "Username *").pack()
+    Entry(screen1, textvariable = username)
+    Label(screen1, text = "Password *").pack()
+    Entry(screen1, textvariable = password)
+    
+def login():
+    print("Login session started")
 
-# manager = pygame_gui.UIManager((window_width, window_height))
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Notes 1.0")
+    Label(text = "Notes 1.0", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
+    Label(text = "").pack()
+    Button(text = "Login", height = "2", width = "30", command = login).pack()
+    Label(text = "").pack()
+    Button(text = "Register", height = "2", width = "30", command = register).pack()
+    
+    screen.mainloop()
 
-def main():
-    print("Hello World!")
-
-main()
+main_screen()
