8e450f1ef854975b2a8d52c2007e56892f954d65
Raune <128294978+anana243@users.noreply.github.com>
2024-01-20 19:07:55 +0000

    log in update

--- screens/login.py
+++ screens/login.py
@@ -1,4 +1,3 @@
-
 # ------
 # what is ur name?
 # [___]
@@ -7,7 +6,6 @@
 # |continue|
 # ------
 
-# 2. 
 
 from tkinter import *
 
@@ -15,7 +13,7 @@ def register_user():
     username_data = username.get()
     password_data = password.get()
     
-    with open(username_data+".txt", "w")as file:
+    with open(username_data, "w")as file:
         file.write(username_data+"\n")
         file.write(password_data)
         
@@ -25,28 +23,56 @@ def register_user():
     Label(window1, text = "Registration Sucessful!", fg = "green", font=("calibri", 11)).pack()
     
 def register():
-    global window1
+    global window1, username, password, username_entry, password_entry
     window1 = Toplevel(window)
     window1.title("Register")
     window1.geometry("300x250")
     
-    global username, password, username_entry, password_entry
     username = StringVar()
     password = StringVar()
     
     Label(window1, text = "Please enter details").pack()
     Label(window1, text = "").pack()
+    
     Label(window1, text = "Username *").pack()
     username_entry = Entry(window1, textvariable = username)
     username_entry.pack()
+    
     Label(window1, text = "Password *").pack()
     password_entry = Entry(window1, textvariable = password)
     password_entry.pack()
     Label(window1, text= "").pack()
+    
     Button(window1, text = "Register", width = 10, height = 1, command = register_user).pack()
     
+def login_user():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+    
 def login():
-    print("Login session started")
+    global window2, username_verify, password_verify, username_entry1, password_entry1
+    
+    window2 = Toplevel(window)
+    window2.title("Login")
+    window2.geometry("300x250")
+    Label(window2, text = "Please enter details").pack()
+    Label(window2, text = "").pack()
+    
+    username_verify = StringVar()
+    password_verify = StringVar()
+    
+    Label(window2, text = "Username *").pack()
+    username_entry1 = Entry(window2, textvariable = username_verify)
+    username_entry1.pack()
+    
+    Label(window2, text = "Password *").pack()
+    password_entry1 =Entry(window2, textvariable = password_verify)
+    password_entry1.pack()
+    Label(window2, text = "").pack()
+    
+    Button(window2, text = "Log in", width = 10, height = 1, command = login_user).pack()
 
 def main_window():
     global window
